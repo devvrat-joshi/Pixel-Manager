b66c903bad5273fb2545ee707797a7b368024845
devvrat-joshi <dsjoshi1990@gmail.com>
2020-10-27 09:56:25 +0530

    correction

--- 1.py
+++ 1.py
@@ -4,120 +4,13 @@ import getpass,sys,signal
 import shutil
 from distutils.dir_util import copy_tree
 from datetime import datetime
+from depend import *
 os.chdir(".")
 menu = os.listdir()
 
 file = "It is a File"
 copy = "Press c to copy the file: "
-search = "Ctrl + S to search: "
-
-def empty_right(stdscr):
-    p,w = stdscr.getmaxyx()
-    per10screen = w//5
-    per = " "*(w//5)
-    stdscr.attron(curses.color_pair(3))
-    for i in range(1,p-2):
-        stdscr.addstr(i,w//5," "*(4*w//5-2))
-
-def print_folder(stdscr,row):
-    try:
-        h = list(os.listdir(row))
-        p,w = stdscr.getmaxyx()
-        per10screen = w//5
-        empty_right(stdscr)
-        for i in range(p-3):
-            stdscr.attron(curses.color_pair(3))
-            if len(h[i])<per10screen:
-                l = h[i]+" "*(per10screen-len(row))
-            else:
-                l = h[i][:per10screen]
-            x = w//5+1
-            y = i+1
-            stdscr.addstr(y,x,l)
-    except:
-        pass
-
-
-def print_menu(stdscr,listings,n,this):
-    global menu
-    # stdscr.clear()
-    h,w = stdscr.getmaxyx()
-    per10screen = w//5
-    maxi = 0
-    per = " "*(w//5)
-    for i in range(1,h-2):
-        stdscr.attron(curses.color_pair(2))
-        stdscr.addstr(i,0,per)
-    stdscr.attroff(curses.color_pair(2))
-    curses.init_pair(4, curses.COLOR_WHITE, 2)
-    curses.init_pair(5, 3,17)
-    stdscr.attron(curses.color_pair(5))
-    stdscr.addstr(0,0," "*w)
-    if listings[0]:
-        print_folder(stdscr,menu[0])
-    else:
-        empty_right(stdscr)
-        stdscr.addstr(h//2,w//2-len(file)//2,file,curses.color_pair(3))
-    stdscr.attron(curses.color_pair(4))
-    stdscr.addstr(h-2,0," "*(w-1))
-    stdscr.addstr(h-2,w//2-len(menu[0])//2,menu[0])
-    for idx, row in enumerate(menu):
-        if (idx==0 and n==0) or this==row:
-            stdscr.attron(curses.color_pair(1))
-            if len(row)<per10screen:
-                i = row+" "*(per10screen-len(row))
-            else:
-                i = row[:per10screen]
-            x = 0
-            y = idx+1
-            stdscr.addstr(y,x,i)
-            stdscr.attroff(curses.color_pair(1))
-        else:
-            if idx>=h-3:
-                break
-            stdscr.attron(curses.color_pair(2))
-            if len(row)<per10screen:
-                i = row+" "*(per10screen-len(row))
-            else:
-                i = row[:per10screen]
-            x = 0
-            y = idx+1
-            stdscr.addstr(y,x,i)
-    stdscr.refresh()
-
-def scrolldown(stdscr,cur_row):
-    global menu
-    h,w = stdscr.getmaxyx()
-    per10screen = w//5
-    maxi = 0
-    per = " "*(w//5)
-    stdscr.attron(curses.color_pair(2))
-    for idx in range(cur_row-h+3,cur_row):
-        if len(menu[idx])<per10screen:
-            i = menu[idx]+" "*(per10screen-len(menu[idx]))
-        else:
-            i = menu[idx][:per10screen]
-        if idx==cur_row-1:
-            stdscr.attron(curses.color_pair(1))
-            if len(menu[idx])<per10screen:
-                i = menu[idx]+" "*(per10screen-len(menu[idx]))
-            else:
-                i = menu[idx][:per10screen]
-            x = 0
-            y = idx+1-cur_row+h-3
-            stdscr.addstr(y,x,i)
-            stdscr.attroff(curses.color_pair(1))
-            stdscr.attron(curses.color_pair(4))
-            stdscr.addstr(h-2,0," "*(w-1))
-            stdscr.addstr(h-2,w//2-len(menu[cur_row-1])//2,menu[cur_row-1])
-        else:
-            x = 0
-            y = idx+1-cur_row+h-3
-            stdscr.addstr(y,x,i)
-    stdscr.refresh()
-
-def stat(stdscr,listings,menu,cur_row):
-    pass
+options = "m : move, k : create file, g : create folder"
 
 def main(stdscr):
     global menu
@@ -131,7 +24,7 @@ def main(stdscr):
     curses.init_pair(2, curses.COLOR_WHITE, 18)
     curses.init_pair(6, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(7,curses.COLOR_WHITE, 57)
-    curses.init_pair(8,curses.COLOR_WHITE, 105)
+    curses.init_pair(8,curses.COLOR_WHITE, 57)
     curses.init_pair(9,curses.COLOR_WHITE, 7)
     cur_row = 1
     maxi = 0
@@ -141,7 +34,7 @@ def main(stdscr):
     curses.init_pair(3, 3, 27)
     stdscr.bkgd(' ', curses.color_pair(3)|curses.A_BOLD)
     path = getpass.getuser()+":"+os.getcwd()+"$"
-    print_menu(stdscr,listings,0,"")
+    print_menu(stdscr,listings,0,"",menu)
     stdscr.addstr(0,w//2-len(path)//2,path,curses.color_pair(5) + curses.A_BOLD)
     terminal = 0
     k = 0
@@ -151,7 +44,7 @@ def main(stdscr):
     date = str(time.ctime())
     stdscr.addstr(1,w-len(date),date,curses.color_pair(5))
     stdscr.addstr(h-1,0," "*(w-1),curses.color_pair(7))
-    stdscr.addstr(h-1,3*w//5-1,search+" "*(2*w//5-len(search)+1),curses.color_pair(8))
+    stdscr.addstr(h-1,3*w//5-1,options+" "*(2*w//5-len(options)+1),curses.color_pair(8))
     stdscr.addstr(h-1,0,copy,curses.color_pair(7))
     while 1:
         enter = 0
@@ -193,7 +86,7 @@ def main(stdscr):
             listings = []
             for i in menu:
                 listings.append(os.path.isdir(i))
-            print_menu(stdscr,listings,0,folder)
+            print_menu(stdscr,listings,0,folder,menu)
             l = len(menu)
             stdscr.addstr(0,0," "*w,curses.color_pair(5))
             stdscr.addstr(0,w//2-len(path)//2,path,curses.color_pair(5))
@@ -227,7 +120,7 @@ def main(stdscr):
                 listings = []
                 for i in menu:
                     listings.append(os.path.isdir(i))
-                print_menu(stdscr,listings,1,old_menu)
+                print_menu(stdscr,listings,1,old_menu,menu)
                 print_folder(stdscr,old_menu)
                 if cur_row<=l:
                     stdscr.attron(curses.color_pair(4))
@@ -348,14 +241,12 @@ def main(stdscr):
             os.chdir("..")
             path = getpass.getuser()+":"+os.getcwd()+"$"
             menu = os.listdir()
-            for i in range(len(menu)):
-                menu[i] = menu[i].lower()
-            cur_row = menu.index(old_menu.lower())+1
+            cur_row = menu.index(old_menu)+1
             l = len(menu)
             listings = []
             for i in menu:
                 listings.append(os.path.isdir(i))
-            print_menu(stdscr,listings,1,old_menu)
+            print_menu(stdscr,listings,1,old_menu,menu)
             print_folder(stdscr,old_menu)
             if cur_row<=l:
                 stdscr.attron(curses.color_pair(4))
@@ -388,7 +279,7 @@ def main(stdscr):
             listings = []
             for i in menu:
                 listings.append(os.path.isdir(i))
-            print_menu(stdscr,listings,0,"")
+            print_menu(stdscr,listings,0,"",menu)
             if terminal==1:
                 stdscr.addstr(0,0,"Terminal: "+path+" "*(w-len(path)-10),curses.color_pair(6))
                 stdscr.attron(curses.color_pair(6))
